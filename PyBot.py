7c53dda19c8fc0efeba676b28ca1787cbf3054a0
Flailfish <Flailfish@users.noreply.github.com>
2020-04-23 21:36:20 -0400

    Update PyBot.py

--- PyBot.py
+++ PyBot.py
@@ -10,12 +10,9 @@ import twitter
 import filehandling
 import keycrypto
 
-
 def main():     
-        
-    
+ 
     rdb = DevilBot.RipDevilBot(r'fred.dat',r'devillastmention.dat',r'lastresponse.dat',r'devilmedia.dat')
-    rdb.execute()
-    
+    rdb.execute()   
     
 main()
